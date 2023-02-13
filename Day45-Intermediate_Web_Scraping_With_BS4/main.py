56400876f3f2c1e41bccf9369248aa87f53e62a2
Luis Colmenarez <lacv@MacBook-Pro-de-Luis.local>
2023-02-13 10:25:18 -0600

    Adding a new py file and scraping some real world pages

--- Day45-Intermediate_Web_Scraping_With_BS4/main.py
+++ Day45-Intermediate_Web_Scraping_With_BS4/main.py
@@ -11,3 +11,16 @@ soup = BeautifulSoup(contents, "html.parser")
 # print(soup.prettify())
 
 print(soup.a)
+
+all_anchor_tags =  soup.find_all(name="a")
+
+for tag in all_anchor_tags:
+    print(tag.getText())
+    print(tag.get("href"))
+    
+heading = soup.find(name="h1", id="name")
+company_url = soup.select_one("p a")
+print(company_url)
+
+headings = soup.select(".heading") 
+print(headings)
