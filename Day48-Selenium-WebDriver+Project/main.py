0ce0348a9c65afe5f5c73a052f7e8b36d7d285c9
Luis Colmenarez <lacv@MacBook-Pro-de-Luis.local>
2023-02-15 13:36:11 -0600

    Starting with selenium and automating webpages interworking2

--- Day48-Selenium-WebDriver+Project/main.py
+++ Day48-Selenium-WebDriver+Project/main.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-chrome_driver_path = "/Users/lacv/Development/chromedriver_mac64/chromedriver"
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("https://www.amazon.com")
