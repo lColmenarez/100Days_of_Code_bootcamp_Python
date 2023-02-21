7059882724e2e158840f695d5eaedf60cdfdb844
Luis Colmenarez <lacv@MacBook-Pro-de-Luis.local>
2023-02-21 16:13:13 -0600

    Flask fist porject

--- Day54-Introduction_to_web_dev_with_Flask/hello.py
+++ Day54-Introduction_to_web_dev_with_Flask/hello.py
@@ -4,3 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_word():
     return "Hello World!"
+
+@app.route('/bye')
+def bye():
+    return "Bye!"
+
+if __name__ == '__main__':
+    app.run()
