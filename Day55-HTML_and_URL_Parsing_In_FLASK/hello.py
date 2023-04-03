9df08cf012084ff000d52932c680ba50389c92b6
Luis Colmenarez <lacv@MacBook-Pro-de-Luis.local>
2023-04-03 17:12:41 -0600

    Flask URL Parsing and HTML rendering + day2

--- Day55-HTML_and_URL_Parsing_In_FLASK/hello.py
+++ Day55-HTML_and_URL_Parsing_In_FLASK/hello.py
@@ -3,11 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_word():
-    return "Hello World!"
+    return "<h1 style='text-align: center'>Hello World!</h1>" \
+           "<p>This is a paragraph.</p>" \
+           "<img src='https://media.giphy.com/media/PxHEc3YRy3cfRuNBVE/giphy.gif' width=200>"
 
 @app.route('/bye')
 def bye():
-    return "Bye!"
+    return "<u><em><b>Bye!</b></em></u>"
+
+@app.route("/username/<path:name>/<int:number>")
+def greet(name, number):
+    return f"Hello there {name}, you are {number} yerars old!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
